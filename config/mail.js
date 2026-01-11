5e2d1a03019ac02f94bbfa2ab503a130f639f611
Bijaya Sigdel <sigdelbijaya385@gmail.com>
2026-01-11 12:15:24 +0545

    Describe your changes

--- config/mail.js
+++ config/mail.js
@@ -4,8 +4,8 @@ const nodemailer = require("nodemailer");
 const transporter = nodemailer.createTransport({
   service: "gmail", // Gmail SMTP
   auth: {
-    user: process.env.sigdelbijaya385@gmail.com,
-    pass: process.env.wkdw hkuu izmy mhxm, // Gmail App Password
+    user: process.env.EMAIL_USER, // Your Gmail address
+    pass: process.env.EMAIL_PASS, // Gmail App Password
   },
 });
 
